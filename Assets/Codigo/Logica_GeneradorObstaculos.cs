bb4079eb13b62434b68b16bc486e94da823b1997
Kevin Verdiguel <kevinvdflrs@gmail.com>
2023-08-07 19:13:48 -0600

    Changes

---
+++ Assets/Codigo/Logica_GeneradorObstaculos.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:61b5bfc6f17bd4b6d130de12a3b7286a4a4f55396287a8e531515881bff817bc
+size 937
---
+++ Assets/Codigo/Logica_Puntaje.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:33a8fed940d9ca91871b1c5ce6b352c43fc907505dca5647a42d9a3007c0a7bb
+size 417
