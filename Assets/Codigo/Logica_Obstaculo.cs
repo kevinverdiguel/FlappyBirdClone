d751b913be93dd64b825b9b91e414e6c2a52abea
Kevin Verdiguel <kevinvdflrs@gmail.com>
2023-08-07 19:07:43 -0600

    Commited program

--- Assets/Codigo/Logica_Obstaculo.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e4671abff4a1f95e2b7d8917a004199e99f59e2e5a9590226907e24b111cd1b5
-size 390
--- Assets/Codigo/Logica_Personaje.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e1603221826a5b519873743f897e21d8b9a359a9e7bd7828ec4639436cd672e8
-size 645
